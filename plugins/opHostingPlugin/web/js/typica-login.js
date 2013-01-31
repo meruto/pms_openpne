7ab13c49a7ace77e0d88f4937342eb1f5d97cd04
suzuki_mar <supasu145@gmail.com>
2013-01-31 17:52:58 +0900

    ログインエラーがダサいのを修正

--- plugins/opHostingPlugin/web/js/typica-login.js
+++ plugins/opHostingPlugin/web/js/typica-login.js
@@ -3,6 +3,13 @@ jQuery(document).ready(function($) {
 	$.backstretch([
       "/opHostingPlugin/bg1.png"
   	], {duration: 3000, fade: 750});
+$('.partsHeading').remove();
+  $('#backLink').remove();
+
+  var errMessage = $('#loginError').text();
+  $('legend').after('<div style="color: #f00; padding-bottom: 10px;">' + errMessage + '</div>');
+
+  $('#loginError').remove();
 		
 });
 
